ffa3f6f02d695207d37ffd02fe075657ff9bd82f
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-07-26 20:55:35 -0300

    Londrina:26.07.23_PlanosEDesignacoes

--- app/Http/Controllers/DesignacaoController.php
+++ app/Http/Controllers/DesignacaoController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Designacao;
 use Illuminate\Http\Request;
 
-class DesignacoesController extends Controller
+class DesignacaoController extends Controller
 {
     /**
      * Display a listing of the resource.
